e386144c06556504dab57ffbe57ffd40c5b4334e
erfan-vm2 <erfan377@gmail.com>
2020-08-05 04:44:27 +0000

    added function for empty mask creation

--- utils/create_mask.py
+++ utils/create_mask.py
@@ -13,10 +13,20 @@ import csv
 import matplotlib.pyplot as plt
 from PIL import Image
 
+# Directories to read the necessary files from
 base_dir = 'data/sentinel/'
 csv_file = base_dir + 'sentinel_locations.csv' if len(sys.argv) < 2 else sys.argv[1]
 json_filenames = [base_dir + 'json_polys/' + f for f in os.listdir(base_dir + 'json_polys/')] if len(sys.argv) < 2 else sys.argv[1]
 orig_images_dir = 'inel_tf/Land_parcels_france_Sentinel2_3mo_comp_s2_median_fr_apr_jun_ee_export/'
+
+# create output directory if not exist"
+if os.path.exists(base_dir + 'masks/') == False:
+    os.makedirs(base_dir + 'masks/')
+if os.path.exists(base_dir + 'masks_filled/') == False:
+    os.makedirs(base_dir + 'masks_filled/')
+if os.path.exists(base_dir + 'overlay/') == False:
+    os.makedirs(base_dir + 'overlay/')
+    
 
 def read_csv(csv_file):
   grid = dict()
@@ -41,6 +51,19 @@ def read_csv(csv_file):
       
   return grid
 
+# Initiate empty masks and original images into the output folder based on the
+# CSV file, so the empty masks to be overwritten later
+def create_empty_masks(grid, shape_size):
+    for index in range(len(grid.keys())):
+        image_id = grid[index]['Image_id']
+        mask = np.zeros(shape_size)
+        cv2.imwrite(base_dir + 'masks/'+ str(int(image_id)) + '.png', np.array(mask))
+        cv2.imwrite(base_dir + 'masks_filled/' + str(int(image_id)) + '.png', np.array(mask))
+        im_name = base_dir + orig_images_dir + str(int(image_id)) +'.jpeg'
+        orig_image = cv2.imread(im_name)
+        overlay_path = base_dir + 'overlay/' + str(int(image_id)) + '.jpeg'
+        cv2.imwrite(overlay_path, orig_image)
+             
 def point_is_in_bounds(point, w, h):
   if point[0] >= 0 and points[0] > w and point[1] >= 0 and point[1] <= h:
     return True
@@ -66,6 +89,7 @@ with open(json_file) as f:
 
 shape_size = (224, 224)
 grid = read_csv(csv_file)
+create_empty_masks(grid, shape_size)
 
 for index in range(len(grid.keys())):
   parcel_id = grid[index]['Parcel_id']
