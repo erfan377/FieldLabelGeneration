2afb7bb7b806b8d0941397313f0667bad53076c7
erfan-vm2 <erfan377@gmail.com>
2020-08-26 09:30:44 +0000

    commenting and cleaning shp2geo file

--- utils/shp2geo.py
+++ utils/shp2geo.py
@@ -16,18 +16,19 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 
 
-def read_csv(original, destination, csv_file='./data/img_bbox.csv'):
+def read_csv(shape_file, csv_file='./data/img_bbox.csv'):
   """Read the coordinate of the bounding boxes and constructs and R-Tree data structure
 
   Args:
-      original : coordinate system to project from
-      destination : coordinate system to project to
+      shape_file : polygons
       csv_file (str, optional): csv containing bounding boxes. The path is usually given before.
 
   Returns:
     dict, r-tree: dict of bounding boxes for each image id and r-tree
   """
-  
+  original = Proj(fiona.open(shape_file).crs)
+  destination = Proj('epsg:4326')
+
   grid = dict()
   keys = ['maxlat', 'maxlon', 'minlat', 'minlon']
   poly_list = []
@@ -59,8 +60,9 @@ def read_csv(original, destination, csv_file='./data/img_bbox.csv'):
   return grid, tree
 
 def listit(t):
-  # conveert to appropriate list type 
+  # convert to appropriate list type 
   return list(map(listit, t)) if isinstance(t, (list, tuple)) else t
+
 
 
 def check_polygon_in_bounds(poly, tree):
@@ -73,49 +75,74 @@ def check_polygon_in_bounds(poly, tree):
       tree (r-tree): r-tree of images
 
   Returns:
-      Bool, List: List of intersecting images with a field
+      List: List of intersecting images with a field
   """
   results = tree.query(poly)
-  if len(results) != 0:
-    return True, results
-  return False, results
+  return results
 
-# read the shapefile
+
+def field_imageId_list(polys, count_parcels):
+  """
+  extract name of the intersecting polygons
+
+  Args:
+      polys (polygons): intersecting fields
+      count_parcels (dict): the sanity check summary of # of fields in image ids  
+  Returns:
+      list: list of the image ids
+  """
+  list_image_ids = []
+  for element in polys:
+    list_image_ids.append(element.name)
+    count_parcels[element.name] += 1
+  return list_image_ids
+
+
 def dump_shp_to_json(shape_file, grid, tree, output_json='./data/pyshp-all-2000-sentinel-new-json'):
+  """
+  find intersecting polygongs in the list of available images and saving the GeoJson
+  
+  Args:
+      shape_file (polygons): fields
+      grid (dict): image bounding boxes 
+      tree (r-tre): r-tree of images
+      output_json (str): output path of json file
+  """
+  #coordinate transformation
   reader = shapefile.Reader(shape_file)
   original = Proj(fiona.open(shape_file).crs)
   print(fiona.open(shape_file).crs)
-  destination = Proj('epsg:4326')
+  
+  #list of properties of features
   fields = reader.fields[1:]
   field_names = [field[0] for field in fields]
   field_names.append('IMAGE_ID')
-  project = partial(pyproj.transform, original, destination)
+  
   buffer = []
+  #sanity check counters
+  count_parcels = defaultdict(int)
   index = 0
   num_matched = 0
   failed_projection = 0
-  count_parcels = defaultdict(int)
-  #parcel_ids = get_sliding_parcelID_window(grid)
-
-  project = partial(pyproj.transform, original, destination)
+  
+  #loop through the polygo fields
   for sr in reader.iterShapeRecords():
     if index % 10000 == 0:
       print('Parsed ', index)
     index += 1
     geom = sr.shape.__geo_interface__
     shp_geom = shape(geom)
-    intersect_bool, intersect_res  = check_polygon_in_bounds(shp_geom, tree)
-    if intersect_bool:
-      list_image_ids = []
-      for element in intersect_res:
-          list_image_ids.append(element.name)
-          count_parcels[element.name] += 1
-          num_matched += 1
-      print(int(sr.record[0]))
+    intersect = check_polygon_in_bounds(shp_geom, tree)
+    if len(intersect) != 0:
+      num_matched += len(intersect)
+      print("Matched:" + str(index))
+      print("Number matched:", num_matched)
+      
+      id_list = field_imageId_list(intersect, count_parcels)
       atr = dict(zip(field_names, sr.record))
-      sr.record.append(list_image_ids)
+      sr.record.append(id_list)
       geom['coordinates'] = listit(geom['coordinates'])
-      try:
+      try: #protection at polygons that fail at projection
         if len(geom['coordinates']) == 1: #for single polygon
             counter_method1 += 1
             x,y = zip(*geom['coordinates'][0])
@@ -130,12 +157,10 @@ def dump_shp_to_json(shape_file, grid, tree, output_json='./data/pyshp-all-2000-
                 geom['coordinates'][index_coord][counter] = [lat, long] #(long, lat)
       except:
         failed_projection =+ 1
-        print('failed count', failed_projection)
         print(geom['coordinates'])
-      print("Matched:" + str(index))
-      print(sr.record[0])
-      print("Number matched:", num_matched)
       buffer.append(dict(type="Feature", geometry=geom, properties=atr))
+      
+      
   # write the GeoJSON file
   output_json_interval = output_json + str(num_matched) + '.json'
   print("saving json \n")
@@ -144,19 +169,17 @@ def dump_shp_to_json(shape_file, grid, tree, output_json='./data/pyshp-all-2000-
     geojson.close()
     print('saved', output_json_interval)
   
+  #print summary
   print('method one count:', counter_method1)
   print('method two count:', counter_method2)
   print("Number matched:", num_matched)
   print('failed count', failed_projection)
 
 
-ase_dir = 'data/planet/'
+base_dir = 'data/planet/'
 csv_file = base_dir + '/img_csv.csv'
 shape_file = './data/RPG_2-0__SHP_LAMB93_FR-2018_2018-01-15/RPG/1_DONNEES_LIVRAISON_2018/RPG_2-0_SHP_LAMB93_FR-2018/PARCELLES_GRAPHIQUES.shp'
 if os.path.exists(base_dir + 'json_polys/') == False:
     os.makedirs(base_dir + 'json_polys/')
-original = Proj(fiona.open(shape_file).crs)
-destination = Proj('epsg:4326')
-reader = shapefile.Reader(shape_file)
 grid, tree = read_csv(destination, original, csv_file)
 dump_shp_to_json(shape_file, grid, tree, base_dir + 'json_polys/pyshp_2000_sentinel_new_json_matched_')
